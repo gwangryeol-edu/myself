0f22270c3e43919638fa9fd849cf65f7ba593adc
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-09-22 13:25:51 +0900

    모듈 학습

--- js/11-module/calculator.js
+++ js/11-module/calculator.js
@@ -16,38 +16,38 @@ import { subtract } from "./math";
 // calculator.js
 
 // 계산기 객체
-// const Calculator = {
-//   add: (a, b) => a + b,
-//   subtract: (a, b) => a - b,
-//   multiply: (a, b) => a * b,
-//   divide: (a, b) => {
-//     if (b === 0) {
-//       throw new Error("0으로 나눌 수 없습니다");
-//     }
-//     return a / b;
-//   },
-// };
+const Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("0으로 나눌 수 없습니다");
+    }
+    return a / b;
+  },
+};
 
-// export default Calculator;
+export default Calculator;
 
 
 // 가장 기본적인 형태의 하나 내보내기(Default Export) 방식
 // 여러 개의 함수(메서드)가 정의된 객체를 생성해서 내보내기
 
 // 겍체란 중괄호로 표현되는 속성의 모음
-const calObject = {
-  add: function (n1, n2) {
-    return n1 + n2;
-  },
+// const calObject = {
+//   add: function (n1, n2) {
+//     return n1 + n2;
+//   },
 
-  subtract: function (n1, n2) {
-    return n1 - n2;
-  },
+//   subtract: function (n1, n2) {
+//     return n1 - n2;
+//   },
 
-  multiplyArrow: (n1, n2)=> {
-    return n1 * n2;
-  }
-}
+//   multiplyArrow: (n1, n2)=> {
+//     return n1 * n2;
+//   }
+// }
 
-// 하나만 내보내기 코드
-export default calObject;
+// // 하나만 내보내기 코드
+// export default calObject;
